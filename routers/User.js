471ba457aa0b2b6c28f2b5c553bff9e2d5d88be9
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-29 18:11:56 +0530

    Add basic Validation

--- routers/User.js
+++ routers/User.js
@@ -6,6 +6,15 @@ const bcrypt = require("bcrypt");
 router.post("/register", async (req, res) => {
   const { username, email, firstname, lastname, password, confirmPassword } =
     req.body;
+  if ((!username, !email, !firstname, !lastname, !password, !confirmPassword)) {
+    return res.json({
+      message: "Please add all the Fields",
+    });
+  }
+  if (password !== confirmPassword) {
+    return res.json({ message: "Password does not match" });
+  }
+
   const user = new User({
     username,
     email,
@@ -14,5 +23,5 @@ router.post("/register", async (req, res) => {
     password,
   });
   await user.save();
-  return res.json({message:'registration Done',user})
+  return res.json({ message: "registration Done", user });
 });
