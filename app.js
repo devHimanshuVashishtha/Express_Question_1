1e597fed1c94cccdd7c98fd6dab100d466950c96
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-29 18:38:14 +0530

    add routing and make proper connection

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const mongoURI = "mongodb+srv://himanshuvashishtha001hp:K3EGwGLdCQaz7Aqe@express1.kvkgela.mongodb.net/?retryWrites=true&w=majority&appName=Express1"
+const mongoURI = "mongodb+srv://himanshuvashishtha001hp:K3EGwGLdCQaz7Aqe@express1.kvkgela.mongodb.net/expressdb1?retryWrites=true&w=majority&appName=Express1"
 const PORT = 3000
+const userRoute = require('./routers/User')
 // K3EGwGLdCQaz7Aqe
+
+app.use(express.json())
+
+app.use('/user',userRoute)
 
 mongoose.connect(mongoURI).then(console.log('MongoDB Connected'))
 
