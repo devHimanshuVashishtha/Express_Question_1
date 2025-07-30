8a02f9387bf9c206b0de3968b43f4dbac82d0e16
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-30 14:10:05 +0530

    Add the feature of taken token with header

--- routers/UserDetails.js
+++ routers/UserDetails.js
@@ -2,8 +2,8 @@ const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
 
-router.get("/get/:token", async (req, res) => {
-  const token = req.params.token;
+router.get("/get", async (req, res) => {
+  const token = req.headers['access_token'];
   if (!token) return res.json({ message: "No token Found" });
   const user = await User.findById(token);
   if (!user) return res.json({ message: "Invalid Token Provided" });
