565d41e3b272abded06c1f24c3d6844bdf104ac3
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-30 13:05:39 +0530

    Add router for getting Details of user

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const mongoURI = process.env.Mongo_URI;
 const PORT = process.env.PORT;
 const userRoute = require("./routers/User");
-const userLogin = require('./routers/UserLogin')
+const userLogin = require("./routers/UserLogin");
+const getDetails = require("./routers/UserDetails");
 
-mongoose.connect(mongoURI).then(()=>console.log("MongoDB Connected"));
-
-
+mongoose.connect(mongoURI).then(() => console.log("MongoDB Connected"));
 
 app.use(express.json());
 
 app.use("/user", userRoute);
-app.use('/user',userLogin)
-
-
+app.use("/user", userLogin);
+app.use("/user", getDetails);
 
 app.listen(PORT, () => {
   console.log(`server Started at http://localhost:${PORT}`);
