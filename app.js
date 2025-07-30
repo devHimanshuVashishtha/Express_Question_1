38ad58b4617e1fe6ab73afb918d6b04aea12c0ce
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-30 12:57:37 +0530

    Correct the basic mistake

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ require('dotenv').config()
 const app = express();
 const mongoURI = process.env.Mongo_URI;
 const PORT = process.env.PORT;
-
 const userRoute = require("./routers/User");
 const userLogin = require('./routers/UserLogin')
+
+mongoose.connect(mongoURI).then(()=>console.log("MongoDB Connected"));
 
 
 
 app.use(express.json());
 
 app.use("/user", userRoute);
-app.use('/userlogin',userLogin)
-mongoose.connect(mongoURI).then(console.log("MongoDB Connected"));
+app.use('/user',userLogin)
+
+
 
 app.listen(PORT, () => {
   console.log(`server Started at http://localhost:${PORT}`);
