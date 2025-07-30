3aa7aa32568caefd20a6cece4473b0b2169fc162
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-30 15:19:35 +0530

    Fix the issue of getting ID

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,10 +1,10 @@
-const express = require("express");
+
 const User = require("../models/user");
 
 async function verification(req, res, next) {
   const token = req.headers["access_token"];
   if (!token) return res.json({ message: "Please Provide token" });
-  const user = await findOne(token);
+  const user = await User.findById(token);
   if (!user) return res.json({ message: "Wrong Token Provided" });
   req.user = user;
   next();
