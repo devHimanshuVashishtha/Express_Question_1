d575aee96e5f270c70c36a33ad3af19a7c9bced0
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 12:58:00 +0530

    add the tokengeneration using uuid in login file

--- routers/UserLogin.js
+++ routers/UserLogin.js
@@ -2,23 +2,47 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const AccessToken = require("../models/access_token");
+const { v4: uuid } = require("uuid");
+const access_token = require("../models/access_token");
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  if ((!username || !password))
+  if (!username || !password)
     return res.status(500).json({ message: "Please Enter all the field" });
-try{
-  const checkUserName = await User.findOne({ username });
-  if (!checkUserName)
-    return res.status(500).json({ message: "No username Found Please register" });
-  const checkPassword = await bcrypt.compare(password,checkUserName.password)
-  if(!checkPassword)
-    return res.status(500).json({message:"Invalid Password"})
-  return res.status(200).json({Access_Token:checkUserName._id})
-}catch(err){
-    console.error(err)
-    return res.status(500).json({message:'Server Error',error:err.message})
-}
+  try {
+    const checkUserName = await User.findOne({ username });
+    if (!checkUserName)
+      return res
+        .status(500)
+        .json({ message: "No username Found Please register" });
+    const checkPassword = await bcrypt.compare(
+      password,
+      checkUserName.password
+    );
+    if (!checkPassword)
+      return res.status(500).json({ message: "Invalid Password" });
+    const accesstoken = uuid();
+    const expiry = new Date(Date.now() + 1000 * 60 * 60);
+
+    await AccessToken.create({
+      user_id: checkUserName.id,
+      access_token: accesstoken,
+      expiry,
+    });
+    return res
+      .status(200)
+      .json({
+        message: "Login Successfully",
+        access_token: accesstoken,
+        expiry_at: expiry,
+      });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ message: "Server Error", error: err.message });
+  }
 });
 
 module.exports = router;
