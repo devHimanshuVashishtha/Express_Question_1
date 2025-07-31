fe41e1ef23e4f78d81bc5f3bdd3d30ea4e3ad031
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 17:40:13 +0530

    create the Schema for adding Address

--- models/addressModel.js
+++ models/addressModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const AddressModel = mongoose.Schema({
-  user_id: { type: mongoose.Schema.Type.ObjectId, ref: "user", require: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
   address: { type: String, require: true },
   city: { type: String, require: true },
   state: { type: String, require: true },
