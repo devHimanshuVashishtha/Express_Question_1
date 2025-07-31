04489dc0c8d273518c793a7d870ef6e32303f37a
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 13:08:13 +0530

    modify the middleWare file for token generation

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,11 +1,14 @@
-
+const AccessToken = require("../models/access_token");
 const User = require("../models/user");
 
 async function verification(req, res, next) {
   const token = req.headers["access_token"];
   if (!token) return res.json({ message: "Please Provide token" });
-  const user = await User.findById(token);
-  if (!user) return res.json({ message: "Wrong Token Provided" });
+  const Access_Token = await AccessToken.findById({ access_token: token });
+  if (!Access_Token) return res.json({ message: "Wrong Token Provided" });
+  if (Access_Token.expiry < new Date()) res.json({ message: "token Expired" });
+  const user = await User.userById(Access_Token.user_id);
+  if(!user) return res.json({message:'No user Found'})
   req.user = user;
   next();
 }
