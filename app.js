3b4a6dbcbed5390728e353ae0519e206c5af92e3
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 17:36:23 +0530

    add the Router for Address section

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ const userLogin = require("./routers/UserLogin");
 const getDetails = require("./routers/UserDetails");
 const deleteData = require("./routers/DeleteData");
 const paginationRoute = require("./routers/pagination");
+const UserAddress = require("./routers/UserAddress");
+// const bodyParser = require("body-parser");
 
 mongoose.connect(mongoURI).then(() => console.log("MongoDB Connected"));
-
+// app.use(bodyParser.json());
 app.use(express.json());
+// app.use(bodyParser.json());
 
 app.use("/user", userRoute);
 app.use("/user", userLogin);
 app.use("/user", getDetails);
 app.use("/user", deleteData);
 app.use("/user", paginationRoute);
+app.use("/user", UserAddress);
 
 app.listen(PORT, () => {
   console.log(`server Started at http://localhost:${PORT}`);
