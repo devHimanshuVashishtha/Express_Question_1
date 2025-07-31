3a821da87771368692dab5f4138115e72fb9f91d
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 17:47:29 +0530

    Adding the feature of fetching details with verifying token by passing id as param and accessToken in headers

--- routers/UserDetails.js
+++ routers/UserDetails.js
@@ -3,8 +3,16 @@ const User = require("../models/user");
 const verification = require("../middleware/verifyToken");
 const router = express.Router();
 
-router.get("/get", verification, (req, res) => {
-    res.json({message:'Token Verify',user:req.user})
+router.get("/get/:id", verification, async (req, res) => {
+    const userId = req.params.id;
+    if (!userId) return res.json({ message: "ID is not provided" });
+    console.log(userId);
+    const user = await User.findById(userId).populate("addresses");
+    console.log(user);
+    if (!user) return res.json({ message: "user not found" });
+    return res.json({ message: "Found", user });
+
+//   return res.json({ message: "Token Verify", user: req.user });
 });
 
 module.exports = router;
