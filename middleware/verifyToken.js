b50a2ae6939220dd7e7ebbdf29d9fdbb910603fe
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 13:43:33 +0530

    modify the verifyToken/improve the mistake"

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,10 +4,10 @@ const User = require("../models/user");
 async function verification(req, res, next) {
   const token = req.headers["access_token"];
   if (!token) return res.json({ message: "Please Provide token" });
-  const Access_Token = await AccessToken.findById({ access_token: token });
+  const Access_Token = await AccessToken.findOne({ access_token: token });
   if (!Access_Token) return res.json({ message: "Wrong Token Provided" });
   if (Access_Token.expiry < new Date()) res.json({ message: "token Expired" });
-  const user = await User.userById(Access_Token.user_id);
+  const user = await User.findById(Access_Token.user_id);
   if(!user) return res.json({message:'No user Found'})
   req.user = user;
   next();
