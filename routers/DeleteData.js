2e14377058cb4cbd900a459625527bc55829e73b
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 19:17:53 +0530

    Delete session when data is deleted

--- routers/DeleteData.js
+++ routers/DeleteData.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router.put("/delete", verification, async (req, res) => {
   const userid = req.user.id;
-  const deleteUser = await User.findByIdAndDelete(userid);
-  res.json({ message: "successfully deleted",deleteUser});
+  await User.findByIdAndDelete(userid);
+  req.session.destroy();
+  res.json({ message: "successfully deleted"});
 });
 
 module.exports = router;
