2ae6308b42295a91f3bfb095defe98118122d270
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-31 17:37:28 +0530

    modified it and resolve the issue

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,15 +2,32 @@ const AccessToken = require("../models/access_token");
 const User = require("../models/user");
 
 async function verification(req, res, next) {
-  const token = req.headers["access_token"];
-  if (!token) return res.json({ message: "Please Provide token" });
-  const Access_Token = await AccessToken.findOne({ access_token: token });
-  if (!Access_Token) return res.json({ message: "Wrong Token Provided" });
-  if (Access_Token.expiry < new Date()) return res.json({ message: "token Expired" });
-  const user = await User.findById(Access_Token.user_id);
-  if (!user) return res.json({ message: "No user Found" });
-  req.user = user;
-  next();
+  try {
+    const token = req.headers["access_token"];
+    if (!token) {
+      return res.status(401).json({ message: "Access token is required" });
+    }
+
+    const accessTokenDoc = await AccessToken.findOne({ access_token: token });
+    if (!accessTokenDoc) {
+      return res.status(403).json({ message: "Invalid access token" });
+    }
+
+    if (accessTokenDoc.expiry < new Date()) {
+      return res.status(403).json({ message: "Access token has expired" });
+    }
+
+    const user = await User.findById(accessTokenDoc.user_id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user; 
+    next(); 
+  } catch (error) {
+    console.error("Verification middleware error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 module.exports = verification;
