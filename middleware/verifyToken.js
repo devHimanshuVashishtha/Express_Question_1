bce34b804250cf0f1c80807a2e95b671d3dac118
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-04 19:36:19 +0530

    Remove the comment form the code

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,30 +1,8 @@
-// const AccessToken = require("../models/access_token");
+
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 async function verification(req, res, next) {
-  // try {
-  // const token = req.headers["access_token"];
-  // if (!token) {
-  //   return res.status(401).json({ message: "Access token is required" });
-  // }
-
-  // const accessTokenDoc = await AccessToken.findOne({ access_token: token });
-  // if (!accessTokenDoc) {
-  //   return res.status(403).json({ message: "Invalid access token" });
-  // }
-
-  // if (accessTokenDoc.expiry < new Date()) {
-  //   return res.status(403).json({ message: "Access token has expired" });
-  // }
-
-  // const user = await User.findById(accessTokenDoc.user_id);
-  // if (!user) {
-  //   return res.status(404).json({ message: "User not found" });
-  // }
-
-  // req.user = user;
-  // next();
 
   const authHeader = req.headers["authorization"];
 
