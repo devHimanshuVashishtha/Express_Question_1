39ddfbba7822bda72db4015769f9bdb8569d50fb
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-05 17:26:20 +0530

    update the file for filter the product category for smartPhone

--- axiosPratice/categorydetail.js
+++ axiosPratice/categorydetail.js
@@ -3,15 +3,15 @@ const fetch = require("node-fetch");
 const router = express.Router();
 
 router.get("/fetch/flipkart/mobile", async (req, res) => {
-  const response = await fetch("https://dummyjson.com/products");
+  const response = await fetch("https://dummyjson.com/products?limit=194");
   const data = await response.json();
   const productdetails = data.products;
-  const groceries = productdetails.filter(
-    (product) => product.category === "groceries"
+  const smartphones = productdetails.filter(
+    (product) => product.category === "smartphones"
   );
   res.json({
-    category: "groceries",
-    product: groceries,
+    category: "smartphones",
+    product: smartphones,
   });
 });
 
