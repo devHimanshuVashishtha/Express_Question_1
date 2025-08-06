fa37f733c68efde6bb36e6ad7c70ca406942e7fa
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-06 11:52:00 +0530

    add the try-catch

--- axiosPratice/NAVfetching.js
+++ axiosPratice/NAVfetching.js
@@ -4,23 +4,27 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 
 router.get("/fetch/nav/:scheme_code", async (req, res) => {
-  const  code = req.params.scheme_code;
-  const url = `https://nav-indian-mutual-fund.p.rapidapi.com/nav?scheme_code=${code}`;
+  try {
+    const code = req.params.scheme_code;
+    const url = `https://nav-indian-mutual-fund.p.rapidapi.com/nav?scheme_code=${code}`;
 
-  const option = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": process.env.RAPID_KEY,
-      "X-RapidAPI-Host": "nav-indian-mutual-fund.p.rapidapi.com",
-    },
-  };
-  const response = await fetch(url, option);
-  const data = await response.json();
-  const data1 = data.data.NAV;
-  res.json({
-    message: "done",
-    NAV: data1,
-  });
+    const option = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.RAPID_KEY,
+        "X-RapidAPI-Host": "nav-indian-mutual-fund.p.rapidapi.com",
+      },
+    };
+    const response = await fetch(url, option);
+    const data = await response.json();
+    const data1 = data.data.NAV;
+    res.json({
+      message: "done",
+      NAV: data1,
+    });
+  } catch (err) {
+    res.json({ message: "server Error", Error: err.message });
+  }
 });
 
 module.exports = router;
--- axiosPratice/categorydetail.js
+++ axiosPratice/categorydetail.js
@@ -3,16 +3,20 @@ const fetch = require("node-fetch");
 const router = express.Router();
 
 router.get("/fetch/flipkart/mobile", async (req, res) => {
-  const response = await fetch("https://dummyjson.com/products?limit=194");
-  const data = await response.json();
-  const productdetails = data.products;
-  const smartphones = productdetails.filter(
-    (product) => product.category === "smartphones"
-  );
-  res.json({
-    category: "smartphones",
-    product: smartphones,
-  });
+  try {
+    const response = await fetch("https://dummyjson.com/products?limit=194");
+    const data = await response.json();
+    const productdetails = data.products;
+    const smartphones = productdetails.filter(
+      (product) => product.category === "smartphones"
+    );
+    res.json({
+      category: "smartphones",
+      product: smartphones,
+    });
+  } catch (err) {
+    res.json({ message: "faild to get the response", Error: err.message });
+  }
 });
 
 module.exports = router;
